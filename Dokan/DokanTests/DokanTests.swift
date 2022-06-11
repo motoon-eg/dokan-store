b74793459ac17aaf85e4e111b94cd55216d756b2
Ahmed M. Hassan <ahmdmhasn@gmail.com>
2022-06-11 14:03:00 +0200

    Remove false assertion

--- Dokan/DokanTests/DokanTests.swift
+++ Dokan/DokanTests/DokanTests.swift
@@ -23,7 +23,6 @@ class DokanTests: XCTestCase {
         // Any test you write for XCTest can be annotated as throws and async.
         // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
         // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-        assert(false)
     }
 
     func testPerformanceExample() throws {
