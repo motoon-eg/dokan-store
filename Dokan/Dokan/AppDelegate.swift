59090be8d9477cd01ccaf560c3c72bf3c5a2c7ec
Ahmed M. Hassan <ahmdmhasn@gmail.com>
2022-06-17 16:32:39 +0200

    Add firebase analytics and crashlytics packages

--- Dokan/Dokan/AppDelegate.swift
+++ Dokan/Dokan/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import FirebaseCore
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +15,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        FirebaseApp.configure()
+        
         return true
     }
 
