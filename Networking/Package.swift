ec93e2846676328ab6954f532c80c2cb004954bb
Ahmed M. Hassan <ahmdmhasn@gmail.com>
2022-06-18 19:29:13 +0200

    Add moya dependency

--- Networking/Package.swift
+++ Networking/Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.5
+// swift-tools-version: 5.6
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -12,13 +12,12 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "Networking",
-            targets: ["Networking"]
-        ),
+            targets: ["Networking"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/Alamofire/Alamofire.git", .upToNextMajor(from: "5.6.1")),
+        .package(url: "https://github.com/Moya/Moya.git", .upToNextMajor(from: "15.0.0"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -26,12 +25,10 @@ let package = Package(
         .target(
             name: "Networking",
             dependencies: [
-                "Alamofire",
-            ]
-        ),
+                "Moya",
+            ]),
         .testTarget(
             name: "NetworkingTests",
-            dependencies: ["Networking"]
-        ),
+            dependencies: ["Networking"]),
     ]
 )
