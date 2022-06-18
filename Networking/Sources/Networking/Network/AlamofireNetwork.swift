6fa8df7e9fc1be13eaeb056beb98d6febab5c2d2
Ahmed M. Hassan <ahmdmhasn@gmail.com>
2022-06-18 19:28:27 +0200

    Add product example

--- Networking/Sources/Networking/Network/AlamofireNetwork.swift
+++ Networking/Sources/Networking/Network/AlamofireNetwork.swift
@@ -11,13 +11,13 @@ import Alamofire
 
 /// AlamofireWrapper: Encapsulates all of the Alamofire OP's
 ///
-class AlamofireNetwork: Network {
+public class AlamofireNetwork: Network {
     
     public init() { }
     
     /// Executes the specified Network Request. Upon completion, the payload will be sent back to the caller as a Data instance.
     ///
-    func responseData(for request: URLRequestConvertible, completion: @escaping (Result<Data, Error>) -> Void) {
+    public func responseData(for request: URLRequestConvertible, completion: @escaping (Result<Data, Error>) -> Void) {
         AF.request(request).responseData { response in
             completion(response.result.toSwiftResult())
         }
@@ -26,7 +26,7 @@ class AlamofireNetwork: Network {
     /// Executes the specified Network Request. Upon completion, the payload or error will be emitted to the publisher.
     /// Only one value will be emitted and the request cannot be retried.
     ///
-    func responseDataPublisher(for request: URLRequestConvertible) -> AnyPublisher<Result<Data, Error>, Never> {
+    public func responseDataPublisher(for request: URLRequestConvertible) -> AnyPublisher<Result<Data, Error>, Never> {
         return Future { promise in
             AF.request(request).responseData { response in
                 let result = response.result.toSwiftResult()
