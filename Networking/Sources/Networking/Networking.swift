da3d74f013bd9f0c4d36edc7c3e1d041771356d9
MarwaEbrahem <marwa15shanab@gmail.com>
2022-06-18 09:27:56 -0700

    feat: Add Network Layer Files

--- Networking/Sources/Networking/Networking.swift
+++ Networking/Sources/Networking/Networking.swift
@@ -1,6 +1,49 @@
+import Alamofire
+import Foundation
+
 public struct Networking {
     public private(set) var text = "Hello, World!"
 
     public init() {
+    }
+    
+    public func perform<M: Decodable, T: TargetType>(target: T, responseClass: M.Type, completion: @escaping (Result<M?,NSError>)->()){
+        let method = Alamofire.HTTPMethod(rawValue: target.method.rawValue)
+        let headers = Alamofire.HTTPHeaders(target.headers ?? [:])
+        let params = buildParams(task: target.task)
+        
+        
+        AF.request(target.baseURL + target.path, method: method, parameters: params.0,encoding: params.1, headers: headers).responseJSON { (response) in
+            guard let statusCode = response.response?.statusCode else{
+                completion(.failure(NSError()))
+                return
+            }
+            
+            if statusCode == 200 {
+                guard let jsonResponse = try? response.result.get() else{
+                    completion(.failure(NSError()))
+                    return
+                }
+                guard let jsonData = try? JSONSerialization.data(withJSONObject: jsonResponse, options: []) else {
+                    completion(.failure(NSError()))
+                    return
+                }
+                guard let responseObject = try? JSONDecoder().decode(M.self, from: jsonData) else {
+                    completion(.failure(NSError()))
+                    return
+                }
+                completion(.success(responseObject))
+            }else{
+                completion(.failure(NSError()))
+            }
+        }
+    }
+    private func buildParams(task:Task)->([String:Any], ParameterEncoding){
+        switch task {
+        case .requestPlain:
+            return ([:],URLEncoding.default)
+        case .requestParameters(parameters: let params, encoding: let encoding):
+            return (params,encoding)
+        }
     }
 }
---
+++ Networking/Sources/Networking/TargetType.swift
@@ -0,0 +1,29 @@
+//
+//  File.swift
+//  
+//
+//  Created by marwa on 18/06/2022.
+//
+
+import Foundation
+import Alamofire
+
+public enum HTTPMethodd : String {  //to map alamofire http methods so if there any change in alamofire I will change here only
+    case get = "GET"
+    case post = "POST"
+    case put = "PUT"
+    case delete = "DELETE"
+}
+
+public enum Task { //wrapper for my reqeust parameter
+    case requestPlain
+    case requestParameters(parameters:[String:Any],encoding: ParameterEncoding )
+}
+
+public protocol TargetType { //wrapper that carries request properties
+    var baseURL : String {get}
+    var path: String {get}
+    var method:HTTPMethodd {get}
+    var task:Task {get}
+    var headers: [String:String]? {get}
+}
