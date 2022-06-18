6f1f579635c4d0718d085b7398321f7c73e59c72
Ahmed M. Hassan <ahmdmhasn@gmail.com>
2022-06-18 21:59:17 +0200

    Add view controller and xib files

---
+++ Dokan/Dokan/ProductListViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ProductListViewController.swift
+//  Dokan
+//
+//  Created by Ahmed M. Hassan on 18/06/2022.
+//
+
+import UIKit
+
+class ProductListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
