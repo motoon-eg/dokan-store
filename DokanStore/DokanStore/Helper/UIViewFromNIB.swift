ddf00f85c7f43f4f60c319df728080773c121de6
Heba MAD <71231086+HebaMad@users.noreply.github.com>
2022-06-18 23:42:41 +0300

    customView

---
+++ DokanStore/DokanStore/Helper/UIViewFromNIB.swift
@@ -0,0 +1,39 @@
+//
+//  UIViewFromNIB.swift
+//  DokanStore
+//
+//  Created by heba isaa on 18/06/2022.
+//
+
+
+import UIKit
+
+class UIViewFromNib: UIView {
+
+   var contentView: UIView!
+
+   var nibName: String {
+       return String(describing: type(of: self))
+   }
+
+   //MARK:
+   override init(frame: CGRect) {
+       super.init(frame: frame)
+
+       loadViewFromNib()
+   }
+
+   required init?(coder aDecoder: NSCoder) {
+       super.init(coder: aDecoder)
+
+       loadViewFromNib()
+   }
+
+   //MARK:
+   func loadViewFromNib() {
+       contentView = Bundle.main.loadNibNamed(nibName, owner: self, options: nil)?[0] as! UIView
+       contentView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+       contentView.frame = bounds
+       addSubview(contentView)
+   }
+}
