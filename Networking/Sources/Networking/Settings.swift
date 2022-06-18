3657183d09a605c320963815bc33f4c66192a701
Ahmed M. Hassan <ahmdmhasn@gmail.com>
2022-06-18 17:03:59 +0200

    Add networking settings

---
+++ Networking/Sources/Networking/Settings.swift
@@ -0,0 +1,17 @@
+//
+//  File.swift
+//  
+//
+//  Created by Ahmed M. Hassan on 18/06/2022.
+//
+
+import Foundation
+
+/// Networking Settings
+/// 
+enum Settings {
+    
+    /// Base API URL
+    ///
+    static let storeApiBaseURL = "https://fakestoreapi.com"
+}
