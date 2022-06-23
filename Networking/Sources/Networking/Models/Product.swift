eed907ee5b3a05c0b420dd6af1dd5afb3a79430e
omarradwan <oradwan037@gmail.com>
2022-06-23 06:37:40 +0200

    init Decoder

--- Networking/Sources/Networking/Models/Product.swift
+++ Networking/Sources/Networking/Models/Product.swift
@@ -8,13 +8,13 @@
 import Foundation
 
 struct Product: Codable {
-    let id: Int
-    let title: String
-    let price: Double
-    let welcomeDescription: String
-    let category: String
-    let image: String
-    let rating: Rating
+    let id: Int?
+    let title: String?
+    let price: Double?
+    let welcomeDescription: String?
+    let category: String?
+    let image: String?
+    let rating: Rating?
     
     enum CodingKeys: String, CodingKey {
         case id = "id"
@@ -25,9 +25,33 @@ struct Product: Codable {
         case image = "image"
         case rating = "rating"
     }
+    
+    init(from decoder: Decoder) throws {
+        let values = try decoder.container(keyedBy: CodingKeys.self)
+        id = try values.decodeIfPresent(Int.self, forKey: .id)
+        title = try values.decodeIfPresent(String.self, forKey: .title)
+        price = try values.decodeIfPresent(Double.self, forKey: .price)
+        welcomeDescription = try values.decodeIfPresent(String.self, forKey: .welcomeDescription)
+        category = try values.decodeIfPresent(String.self, forKey: .category)
+        image = try values.decodeIfPresent(String.self, forKey: .image)
+        rating = try values.decodeIfPresent(Rating.self, forKey: .rating)
+    }
 }
 
+
 struct Rating: Codable {
-    let rate: Double
-    let count: Int
+    let rate: Double?
+    let count: Int?
+    
+    enum CodingKeys: String, CodingKey {
+        case rate = "rate"
+        case count = "count"
+    }
+    
+    init(from decoder: Decoder) throws {
+        let values = try decoder.container(keyedBy: CodingKeys.self)
+        rate = try values.decodeIfPresent(Double.self, forKey: .rate)
+        count = try values.decodeIfPresent(Int.self, forKey: .count)
+    }
+    
 }
