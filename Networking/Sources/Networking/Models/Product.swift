2468638327985669342785c3e8c249830023d619
omarradwan <oradwan037@gmail.com>
2022-06-23 06:27:31 +0200

    Add Product Model

---
+++ Networking/Sources/Networking/Models/Product.swift
@@ -0,0 +1,33 @@
+//
+//  File.swift
+//  
+//
+//  Created by Omar Ahmed on 23/06/2022.
+//
+
+import Foundation
+
+struct Product: Codable {
+    let id: Int
+    let title: String
+    let price: Double
+    let welcomeDescription: String
+    let category: String
+    let image: String
+    let rating: Rating
+    
+    enum CodingKeys: String, CodingKeys {
+        case id = "id"
+        case title = "title"
+        case price = "price"
+        case welcomeDescription = "welcomeDescription"
+        case category = "category"
+        case image = "image"
+        case rating = "rating"
+    }
+}
+
+struct Rating: Codable {
+    let rate: Double
+    let count: Int
+}
