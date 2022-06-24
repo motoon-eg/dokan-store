2f13d88fbcfe15a35cb452e8423b8826c2544e6c
Heba MAD <71231086+HebaMad@users.noreply.github.com>
2022-06-24 09:51:18 +0300

    add platform version

---
+++ DokanStore/Networking/Package.swift
@@ -0,0 +1,34 @@
+// swift-tools-version: 5.6
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "Networking",
+    platforms: [
+         .macOS(.v10_12),
+         .iOS(.v10),
+     ],
+    products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        .library(
+            name: "Networking",
+            targets: ["Networking"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/Moya/Moya.git", .upToNextMajor(from: "15.0.0"))
+
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        .target(
+            name: "Networking",
+            dependencies: [
+                "Moya",
+            ]),
+        .testTarget(
+            name: "NetworkingTests",
+            dependencies: ["Networking"]),
+    ]
+)
