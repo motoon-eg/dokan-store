7e825514aa412781d70ae745ce77caf485afcdbc
Ahmed M. Hassan <ahmdmhasn@gmail.com>
2022-06-25 17:29:38 +0200

    Fix swiftlint errors

--- Dokan/Dokan/AppDelegate.swift
+++ Dokan/Dokan/AppDelegate.swift
@@ -16,7 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         FirebaseApp.configure()
-        
+
         return true
     }
 
@@ -36,4 +36,3 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
 }
-
--- Dokan/Dokan/SceneDelegate.swift
+++ Dokan/Dokan/SceneDelegate.swift
@@ -49,4 +49,3 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 
 }
-
