6acf116aa60069d144acce4b0974f03fa1f8420c
Ahmed M. Hassan <ahmdmhasn@gmail.com>
2022-06-26 02:51:33 +0200

    add logger needed files

--- Logger/Sources/Logger/Logger.swift
+++ Logger/Sources/Logger/Logger.swift
@@ -1,6 +1,15 @@
-public struct Logger {
-    public private(set) var text = "Hello, World!"
-
+public struct Logger: Logging {
+    public static let `default` = Logger()
+    
     public init() {
+        
+    }
+    
+    public func log(_ message: @autoclosure () -> String,
+                    level: LogLevel,
+                    file: StaticString,
+                    function: StaticString,
+                    line: UInt) {
+        #warning("Implement logger")
     }
 }
