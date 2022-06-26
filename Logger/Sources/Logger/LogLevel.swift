bc18d2f95cfce16932e18554868ff9f9451d8d90
Ahmed M. Hassan <45182214+ahmdmhasn@users.noreply.github.com>
2022-06-26 03:34:36 +0200

    feat: Add Logger Module and Integrate with Project 📝 (#28)
    * Add logger package
    
    * add logger needed files
    
    * Replace foundation project with package and include logger
    
    * Rename logger with logging
    
    * Add logging to main project
    
    * Rename logging to logger

---
+++ Logger/Sources/Logger/LogLevel.swift
@@ -0,0 +1,35 @@
+//
+//  File.swift
+//  
+//
+//  Created by Ahmed M. Hassan on 26/06/2022.
+//
+
+import Foundation
+
+/// Telling how important a given log message is.
+///
+public enum LogLevel {
+    
+    /// When you need full visibility of what is happening in your application.
+    ///
+    case verbose
+    
+    /// should be used for information that may be needed for diagnosing issues and troubleshooting or
+    /// when running application in the test environment for the purpose of making sure everything is
+    /// running correctly
+    case debug
+    
+    /// Standard log level indicating that something happened, the application entered a certain state, etc.
+    ///
+    case info
+    
+    /// Indicates that something unexpected happened in the application, a problem, or a situation that
+    /// might disturb one of the processes. But that doesn’t mean that the application failed.
+    ///
+    case warn
+    
+    /// Should be used when the application hits an issue preventing one or more functionalities from
+    /// properly functioning.
+    case error
+}
