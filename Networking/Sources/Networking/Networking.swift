486ea24fce63e80120bc2ddb189f719e7be68408
MarwaEbrahem <marwa15shanab@gmail.com>
2022-06-28 18:57:20 -0700

    add networking layer

--- Networking/Sources/Networking/Networking.swift
+++ Networking/Sources/Networking/Networking.swift
@@ -1,6 +1,61 @@
+import Foundation
+import Alamofire
 public struct Networking {
-    public private(set) var text = "Hello, World!"
 
     public init() {
+    }
+    
+    public func perform<M: Decodable, T: TargetType>(target: T, responseClass: M.Type, completion: @escaping (Result<M, Error>) -> ()){
+        let method = target.method.httpMethod
+        let headers = Alamofire.HTTPHeaders(target.headers ?? [:])
+        let params = buildParams(task: target.task)
+        AF.request(target.baseURL + target.path, method: method, parameters: params.0,encoding: params.1, headers: headers).response { response in
+            switch response.result {
+            case .failure(let error):
+                completion(.failure(NetworkingError.networkError(error)))
+            case .success(_):
+                guard let jsonResponse = try? response.result.get() else {
+                    completion(.failure(NetworkingError.responseValidationFailed))
+                    return
+                }
+                guard let jsonData = try? JSONSerialization.data(withJSONObject: jsonResponse, options: []) else {
+                    completion(.failure(NetworkingError.responseSerializationFailed))
+                    return
+                }
+                guard let responseObject = try? JSONDecoder().decode(M.self, from: jsonData) else {
+                    completion(.failure(NetworkingError.responseDecoderFailed))
+                    return
+                }
+                completion(.success(responseObject))
+            }
+        }
+    }
+    
+    private func buildParams(task: Task) -> ([String: Any], ParameterEncoding){
+        switch task {
+        case .requestPlain:
+            return ([:],URLEncoding.default)
+        case .requestParameters(parameters: let params, encoding: let encoding):
+            return (params,encoding)
+        }
+    }
+    
+    private enum NetworkingError: LocalizedError {
+        case networkError(Error)
+        case responseValidationFailed
+        case responseSerializationFailed
+        case responseDecoderFailed
+        public var errorDescription: String? {
+            switch self {
+            case .networkError(let error):
+                return "Invalid URL Or Connection error: \(error.localizedDescription)"
+            case .responseValidationFailed:
+                return "Response Validation failed"
+            case .responseSerializationFailed:
+                return "Response serialization failed"
+            case .responseDecoderFailed:
+                return "Response Decoder failed"
+            }
+        }
     }
 }
---
+++ Networking/Sources/Networking/TargetType.swift
@@ -0,0 +1,46 @@
+//
+//  TargetType.swift
+//  
+//
+//  Created by marwa on 28/06/2022.
+//
+
+import Foundation
+import Alamofire
+
+/// Used to map alamofire http methods.
+///
+/// - Note: so if there any change in alamofire, we need to update it here.
+public enum HTTPMethodd {
+    case get
+    case post
+    case put
+    case delete
+    var httpMethod: HTTPMethod {
+        switch self{
+        case .get:
+            return .get
+        case .post:
+            return .post
+        case .put:
+            return .put
+        case .delete:
+            return .delete
+        }
+    }
+}
+
+/// wrapper for my reqeust parameter
+public enum Task {
+    case requestPlain
+    case requestParameters(parameters: [String: Any], encoding: ParameterEncoding)
+}
+
+/// wrapper that carries request properties
+public protocol TargetType {
+    var baseURL: String { get }
+    var path: String { get }
+    var method: HTTPMethodd { get }
+    var task: Task { get }
+    var headers: [String: String]? { get }
+}
