af52ed8629dc72c68d811e3d19c6b384468ef496
omarradwan <oradwan037@gmail.com>
2022-07-01 17:44:44 +0200

    Add MVVM Custom Template

---
+++ Template/MVVM Files Generator.xctemplate/WithoutXIB/___FILEBASENAME___ViewController.swift
@@ -0,0 +1,51 @@
+//  ___FILEHEADER___
+
+import UIKit
+
+class ___FILEBASENAMEASIDENTIFIER___: UIViewController {
+    
+    // MARK: Outlets
+        
+
+
+    // MARK: Properties
+        
+    private let viewModel: ___VARIABLE_productName___ViewModelType
+
+    // MARK: Init
+        
+    init(viewModel: ___VARIABLE_productName___ViewModelType) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    required init?(coder: NSCoder) {
+            fatalError("init(coder:) has not been implemented")
+    }
+
+        // MARK: Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
+
+// MARK: - Actions
+extension ___FILEBASENAMEASIDENTIFIER___ {
+    
+}
+
+// MARK: - Bind ViewModel
+extension ___FILEBASENAMEASIDENTIFIER___ {
+    
+}
+
+//MARK: - Ojective C Functions
+extension ___FILEBASENAMEASIDENTIFIER___ {
+    
+    
+}
+
+
+
---
+++ Template/MVVM Files Generator.xctemplate/WithoutXIB/___FILEBASENAME___ViewModelType.swift
@@ -0,0 +1,14 @@
+//  ___FILEHEADER___
+
+
+import Foundation
+
+typealias ___FILEBASENAMEASIDENTIFIER___ = ___VARIABLE_productName___ViewModelInput & ___VARIABLE_productName___ViewModelOutput
+
+protocol ___VARIABLE_productName___ViewModelInput {
+  
+}
+
+protocol ___VARIABLE_productName___ViewModelOutput {
+
+}
