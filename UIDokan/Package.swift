669e91b3f54009ee968f0440b4b1f9da210949ea
Ahmed M. Hassan <ahmdmhasn@gmail.com>
2022-07-01 20:35:21 +0200

    Update Minimum UITarget to iOS 13

--- UIDokan/Package.swift
+++ UIDokan/Package.swift
@@ -7,7 +7,7 @@ let package = Package(
     name: "UIDokan",
     platforms: [
         .macOS(.v10_12),
-        .iOS(.v10),
+        .iOS(.v13),
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
