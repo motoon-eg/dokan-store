34bf9153fbeedbfcd3b25251e48fb4092cd4a024
Omar R <oradwan037@gmail.com>
2022-07-02 17:54:54 +0200

    Update Template/MVVM Files Generator.xctemplate/WithoutXIB/___FILEBASENAME___ViewModelType.swift
    Co-authored-by: Ahmed M. Hassan <45182214+ahmdmhasn@users.noreply.github.com>

--- Template/MVVM Files Generator.xctemplate/WithoutXIB/___FILEBASENAME___ViewModelType.swift
+++ Template/MVVM Files Generator.xctemplate/WithoutXIB/___FILEBASENAME___ViewModelType.swift
@@ -3,12 +3,18 @@
 
 import Foundation
 
+/// ___VARIABLE_productName___ Input & Output
+///
 typealias ___FILEBASENAMEASIDENTIFIER___ = ___VARIABLE_productName___ViewModelInput & ___VARIABLE_productName___ViewModelOutput
 
+/// ___VARIABLE_productName___ ViewModel Input
+///
 protocol ___VARIABLE_productName___ViewModelInput {
   
 }
 
+/// ___VARIABLE_productName___ ViewModel Output
+///
 protocol ___VARIABLE_productName___ViewModelOutput {
 
 }
