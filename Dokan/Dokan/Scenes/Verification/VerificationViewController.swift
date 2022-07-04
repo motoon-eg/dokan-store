d65639f98c6924166b8786ead52c7ebea4959d50
Ahmed Nagy <ahmednaagy97@gmail.com>
2022-07-04 18:01:08 +0200

    Add Verification-Scene to Dokan Target

---
+++ Dokan/Dokan/Scenes/Verification/VerificationViewController.swift
@@ -0,0 +1,17 @@
+//
+//  VerificationViewController.swift
+//  Dokan
+//
+//  Created by Ahmed Nagy on 04/07/2022.
+//
+
+import UIKit
+
+class VerificationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
