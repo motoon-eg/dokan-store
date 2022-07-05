aefdb013205c5b469f3a8dfc553830eab21ddc6c
Heba MAD <71231086+HebaMad@users.noreply.github.com>
2022-07-05 19:57:24 +0300

    registration screen test

---
+++ Dokan/DokanTests/Scenes/Registratio9nTest/RegisterViewControllerTests.swift
@@ -0,0 +1,42 @@
+//
+//  RegisterViewControllerTests.swift
+//  DokanTests
+//
+//  Created by heba isaa on 05/07/2022.
+//
+
+import XCTest
+@testable import Dokan
+class RegisterViewControllerTests: XCTestCase {
+    
+    var sut:RegisterViewModel!
+    
+    // MARK: - life cycle
+    override func setUpWithError() throws {
+         sut = RegisterViewModel()
+    }
+    
+    override func tearDownWithError() throws {
+        sut = nil
+    }
+    
+    
+    // MARK: - Tests
+    
+    
+    func testConfigureIsButtonEnabled_WhenEmailOrPhoneIsEmpty_ShouldBeDisabled(){
+        //Given
+        
+        var isEnabled:Bool?
+        sut.ConfigureButtonEnabled { _isEnabled in
+            isEnabled=_isEnabled
+        }
+        
+        //When
+        sut.updatePhoneOrEmail(input: "hebaaisaa2000@gmail.com")
+        
+        //Then
+        XCTAssertTrue(isEnabled == true)
+    }
+    
+}
