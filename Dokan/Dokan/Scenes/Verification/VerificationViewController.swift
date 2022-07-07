4a8354a5925a5e0646cc120139494b0c1e7e65d1
Ahmed Nagy <ahmednaagy97@gmail.com>
2022-07-07 20:14:29 +0200

    replace the default view controller with the mvvm template files

--- Dokan/Dokan/Scenes/Verification/VerificationViewController.swift
+++ Dokan/Dokan/Scenes/Verification/VerificationViewController.swift
@@ -1,17 +1,51 @@
-//
+//  
 //  VerificationViewController.swift
 //  Dokan
 //
-//  Created by Ahmed Nagy on 04/07/2022.
+//  Created by Ahmed Nagy on 07/07/2022.
 //
 
 import UIKit
 
 class VerificationViewController: UIViewController {
+    
+    // MARK: Outlets
 
+    // MARK: Properties
+        
+    private let viewModel: VerificationViewModelType
+
+    // MARK: Init
+        
+    init(viewModel: VerificationViewModelType) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    // MARK: Lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
+}
+
+// MARK: - Actions
+//
+extension VerificationViewController {
+    
+}
+
+// MARK: - Configurations
+//
+extension VerificationViewController {
+    
+}
+
+// MARK: - Private Handlers
+//
+private extension VerificationViewController {
 }
