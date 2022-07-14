70f3c73a163d9ef5900d3b25fc9ef72c55de8b89
ziad ali <ziadalise@gmail.com>
2022-07-14 21:38:29 +0200

    add colors and fonts as extentions

---
+++ UIDokan/Sources/UIDokan/Custom colors fonts/Colors+Ext.swift
@@ -0,0 +1,36 @@
+//
+//  File.swift
+//  
+//
+//  Created by ziad on 14/07/2022.
+//
+
+import UIKit
+
+extension UIColor{
+
+    class var blueOcean: UIColor {
+        return UIColor(named: "Blue Ocean")!
+    }
+
+    class var orangeFresh: UIColor {
+        return UIColor(named: "Orange Fresh")!
+    }
+
+    class var navyBlack: UIColor {
+        return UIColor(named: "Navy Black")!
+    }
+
+    class var pureWhite: UIColor {
+        return UIColor(named: "Pure White")!
+    }
+
+    class var redVelvet: UIColor {
+        return UIColor(named: "Red Velvet")!
+    }
+
+    class var halfGrey: UIColor {
+        return UIColor(named: "Half Grey")!
+    }
+
+}
---
+++ UIDokan/Sources/UIDokan/Custom colors fonts/Font+Ext.swift
@@ -0,0 +1,46 @@
+//
+//  File.swift
+//  
+//
+//  Created by ziad on 14/07/2022.
+//
+
+import UIKit
+
+extension UIFont{
+    // MARK: - Bold
+    class var registerTitle: UIFont{
+        return UIFont(name: "DMSans-Bold", size: 25)!
+    }
+
+    class var coverTitle: UIFont{
+        return UIFont(name: "DMSans-Bold", size: 20)!
+    }
+
+    class var productPrice: UIFont{
+        return UIFont(name: "DMSans-Bold", size: 12)!
+    }
+    // MARK: - Meduim
+    class var categoriesTitle: UIFont{
+        return UIFont(name: "DMSans-Medium", size: 16)!
+    }
+
+    class var productTitle_Button_TextFeild: UIFont{
+        return UIFont(name: "DMSans-Medium", size: 14)!
+    }
+
+    class var smallButton: UIFont{
+        return UIFont(name: "DMSans-Medium", size: 12)!
+    }
+
+    // MARK: - Regular
+    class var body: UIFont{
+        return UIFont(name: "DMSans-Regular", size: 14)!
+    }
+
+    class var reviews: UIFont{
+        return UIFont(name: "DMSans-Regular", size: 10)!
+    }
+
+
+}
