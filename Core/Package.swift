68daed8ffac09d3ffd6fd62ce2f6c1cc34abf781
Ahmed M. Hassan <45182214+ahmdmhasn@users.noreply.github.com>
2022-07-14 03:51:11 +0200

    feat: Add `ShipBookSDK` Logger 📚 (#49)
    * Limit project packages to iOS 13
    
    * Add `ShipBookSDK`
    
    * Rename `DefaultLogger` to `ShipBookLogger`
    
    * Dokan internal loggers will use default logger by default
    
    * Create group to hold all loggers

--- Core/Package.swift
+++ Core/Package.swift
@@ -6,8 +6,7 @@ import PackageDescription
 let package = Package(
     name: "Core",
     platforms: [
-        .macOS(.v10_12),
-        .iOS(.v10),
+        .iOS(.v13),
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
