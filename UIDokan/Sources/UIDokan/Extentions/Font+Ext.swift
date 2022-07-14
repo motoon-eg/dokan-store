ca130fc54d2bb916fa82bdfa7073a1602c86bdb2
ziad ali <ziadalise@gmail.com>
2022-07-14 22:45:29 +0200

    change names

---
+++ UIDokan/Sources/UIDokan/Extentions/Font+Ext.swift
@@ -0,0 +1,66 @@
+//
+//  File.swift
+//  
+//
+//  Created by ziad on 14/07/2022.
+//
+
+import UIKit
+
+
+// MARK: - Buttons
+//
+extension UIFont{
+    
+    class public var dokanButton: UIFont{
+        return UIFont(name: "DMSans-Medium", size: 14)!
+    }
+    
+    class public var dokansmallButton: UIFont{
+        return UIFont(name: "DMSans-Medium", size: 12)!
+    }
+}
+
+
+// MARK: - labels
+//
+extension UIFont{
+    
+    class public var dokanTitle1: UIFont{
+        return UIFont(name: "DMSans-Bold", size: 26)!
+    }
+    
+    class public var dokanTitle2: UIFont{
+        return UIFont(name: "DMSans-Bold", size: 20)!
+    }
+    
+    class public var dokanTitle3: UIFont{
+        return UIFont(name: "DMSans-Medium", size: 18)!
+    }
+    
+    class public var dokanCallOut: UIFont{
+        return UIFont(name: "DMSans-Medium", size: 14)!
+    }
+    
+    class public var dokanFootnote: UIFont{
+        return UIFont(name: "DMSans-Bold", size: 12)!
+    }
+
+    class public var dokanBody: UIFont{
+        return UIFont(name: "DMSans-Regular", size: 14)!
+    }
+
+    class public var dokanCaption1: UIFont{
+        return UIFont(name: "DMSans-Regular", size: 10)!
+    }
+}
+
+// MARK: - TextField
+//
+extension UIFont {
+    
+    class public var dokanTextFeild: UIFont{
+        return UIFont(name: "DMSans-Medium", size: 14)!
+    }
+    
+}
