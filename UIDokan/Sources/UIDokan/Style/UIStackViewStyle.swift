b0198585fae15e62e513737621e4a17ceb1cd249
MarwaEbrahem <marwa15shanab@gmail.com>
2022-07-14 12:29:47 -0700

    Add stack view style to two labels

--- UIDokan/Sources/UIDokan/Style/UIStackViewStyle.swift
+++ UIDokan/Sources/UIDokan/Style/UIStackViewStyle.swift
@@ -15,7 +15,13 @@ let textField: UITextField = {
     return content
 }()
 
-let label: UILabel = {
+let titleLabel: UILabel = {
+    let content = UILabel()
+    content.translatesAutoresizingMaskIntoConstraints = false
+    return content
+}()
+
+let bodyLabel: UILabel = {
     let content = UILabel()
     content.translatesAutoresizingMaskIntoConstraints = false
     return content
@@ -25,16 +31,16 @@ extension UIStackView {
     
     private func labelAndTextField(_ labelName: String, _ placeholder: String) {
         
-        addSubview(label)
-        label.topAnchor.constraint(equalTo: topAnchor, constant: 10).isActive = true
-        label.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 10).isActive = true
-        label.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -10).isActive = true
-        label.applyLabelStyle(.textFieldTitle, labelInfo: labelName)
+        addSubview(titleLabel)
+        titleLabel.topAnchor.constraint(equalTo: topAnchor, constant: 0).isActive = true
+        titleLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 0).isActive = true
+        titleLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: 0).isActive = true
+        titleLabel.applyLabelStyle(.textFieldTitle, labelInfo: labelName)
         
         addSubview(textField)
-        textField.topAnchor.constraint(equalTo: label.bottomAnchor, constant: 20).isActive = true
-        textField.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 10).isActive = true
-        textField.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -10).isActive = true
+        textField.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 20).isActive = true
+        textField.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 0).isActive = true
+        textField.trailingAnchor.constraint(equalTo: trailingAnchor, constant: 0).isActive = true
         textField.textFieldStyle(placeholder)
     }
     
@@ -43,6 +49,20 @@ extension UIStackView {
         textField.eye()
     }
     
+    private func titleLabelAndBodyLabel(_ title: String, _ body: String) {
+        addSubview(titleLabel)
+        titleLabel.topAnchor.constraint(equalTo: topAnchor, constant: 0).isActive = true
+        titleLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 0).isActive = true
+        titleLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: 0).isActive = true
+        titleLabel.applyLabelStyle(.title, labelInfo: title)
+        
+        addSubview(bodyLabel)
+        bodyLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 20).isActive = true
+        bodyLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 0).isActive = true
+        bodyLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: 0).isActive = true
+        bodyLabel.applyLabelStyle(.body, labelInfo: body)
+        
+    }
 }
 
 extension UIStackView {
@@ -50,14 +70,17 @@ extension UIStackView {
     public enum stackViewStyle {
         case labelAndTextField
         case labelAndPasswordTextField
+        case titleLabelAndBodyLabel
     }
     
-    public func applyStackViewStyle(_ style: stackViewStyle, labelName: String, placeholder: String) {
+    public func applyStackViewStyle(_ style: stackViewStyle, labelTitle: String, placeholderOrBodyLabel: String) {
         switch style {
         case .labelAndTextField:
-            labelAndTextField(labelName, placeholder)
+            labelAndTextField(labelTitle, placeholderOrBodyLabel)
         case .labelAndPasswordTextField:
-            labelAndPasswordTextField(labelName, placeholder)
+            labelAndPasswordTextField(labelTitle, placeholderOrBodyLabel)
+        case .titleLabelAndBodyLabel:
+            titleLabelAndBodyLabel(labelTitle, placeholderOrBodyLabel)
         }
     }
 }
