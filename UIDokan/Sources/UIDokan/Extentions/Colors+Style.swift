d49be8638a42086621ed49e28ec399f8c64d3045
ziad ali <52021782+ziadali22@users.noreply.github.com>
2022-07-15 20:27:03 +0200

    style: add colors and fonts as extensions (#53)
    * add colors and fonts as extentions
    
    * make var public to use in view controllers
    
    * change names
    
    * change smallButton name
    
    * add offGrey color
    
    * Rename files
    
    * change class to static to prevent override

---
+++ UIDokan/Sources/UIDokan/Extentions/Colors+Style.swift
@@ -0,0 +1,40 @@
+//
+//  Color+Style.swift
+//  
+//
+//  Created by ziad on 14/07/2022.
+//
+
+import UIKit
+
+public extension UIColor {
+
+    static var blueOcean: UIColor {
+        return UIColor(named: "Blue Ocean")!
+    }
+
+    static var orangeFresh: UIColor {
+        return UIColor(named: "Orange Fresh")!
+    }
+
+    static var navyBlack: UIColor {
+        return UIColor(named: "Navy Black")!
+    }
+
+    static var pureWhite: UIColor {
+        return UIColor(named: "Pure White")!
+    }
+
+    static var redVelvet: UIColor {
+        return UIColor(named: "Red Velvet")!
+    }
+
+    static var halfGrey: UIColor {
+        return UIColor(named: "Half Grey")!
+    }
+    
+    static var offGrey: UIColor {
+        return UIColor(named: "Off Grey")!
+    }
+
+}
