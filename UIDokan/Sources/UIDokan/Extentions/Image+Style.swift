48678140e22d3dc6fa85bcc7951532f2a07b488c
ziad ali <ziadalise@gmail.com>
2022-07-15 21:12:34 +0200

    adding images Template

---
+++ UIDokan/Sources/UIDokan/Extentions/Image+Style.swift
@@ -0,0 +1,17 @@
+//
+//  Image+Style.swift
+//  
+//
+//  Created by ziad on 15/07/2022.
+//
+
+import Foundation
+import UIKit
+
+extension UIImage {
+    
+    static var comics: UIImage {
+        return ImageAsset(name: "comics").image
+    }
+    
+}
