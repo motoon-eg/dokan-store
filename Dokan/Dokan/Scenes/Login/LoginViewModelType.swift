3dd7d39d64548472370680cd927a1e903423aca9
claramounir <107275541+claramounir@users.noreply.github.com>
2022-07-19 04:22:35 +0200

    feat/ Create Login View Controller 🎃 (#48)
    * Setup Login (template & unit test)
    
    * Add basic design to LoginViewController
    
    Add basics UI components to the xib file and adjust Autolayout to each component to fit in any device (iphones - ipads)
    
    * Update LoginViewController.swift
    
    * add-view-model-input-output-functions
    
    * Bind ViewModel to view
    
    * Update DokanTests.swift
    
    * Adjust LoginViewModelTests
    
    * Update LoginViewController.xib
    
    * adjust IBOutlet names
    
    * Create LoginViewControllerTests.swift
    
    * Update Dokan/Dokan/Scenes/Login/LoginViewController.swift
    
    Co-authored-by: Ahmed M. Hassan <45182214+ahmdmhasn@users.noreply.github.com>
    
    * Update Dokan/Dokan/Scenes/Login/LoginViewModelType.swift
    
    Co-authored-by: Ahmed M. Hassan <45182214+ahmdmhasn@users.noreply.github.com>
    
    * Update LoginViewController.swift
    
    * Update LoginViewController.swift
    
    * Update LoginViewModel.swift
    
    * Update LoginViewModelTests.swift
    
    * Enhance UI & Constrains
    
    * Update LoginViewController.swift
    
    * Update LoginViewController.swift
    
    * Adjust some constrains
    
    Co-authored-by: omarradwan <oradwan037@gmail.com>
    Co-authored-by: Ahmed M. Hassan <45182214+ahmdmhasn@users.noreply.github.com>
    Co-authored-by: Ahmad Yasser <42544598+ahmdyasser@users.noreply.github.com>

---
+++ Dokan/Dokan/Scenes/Login/LoginViewModelType.swift
@@ -0,0 +1,25 @@
+//  
+//  LoginViewModelType.swift
+//  Dokan
+//
+//  Created by Omar Ahmed on 02/07/2022.
+//
+
+import Foundation
+
+/// Login Input & Output
+///
+typealias LoginViewModelType = LoginViewModelInput & LoginViewModelOutput
+
+/// Login ViewModel Input
+///
+protocol LoginViewModelInput {
+    func updateEmail(_ text: String)
+    func updatePassword(_ text: String)
+}
+
+/// Login ViewModel Output
+///
+protocol LoginViewModelOutput {
+    func configureOnButtonEnabled(onEnabled: @escaping (Bool) -> Void)
+}
