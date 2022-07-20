91ba3982a69f8bb36de1ad4c68bad7d7c5bfbaba
ziad ali <ziadalise@gmail.com>
2022-07-20 17:25:08 +0200

    make custom template for swiftGen for Images and Colors

--- Dokan/Dokan/ViewController.swift
+++ Dokan/Dokan/ViewController.swift
@@ -12,8 +12,6 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        let imageContent = Asset.comics.image
-        let blueOceanColor = ColorName.blueOcean.color
         
     }
 
---
+++ UIDokan/Sources/UIDokan/Extentions/Images+Style.swift
@@ -0,0 +1,10 @@
+import UIKit.UIImage
+
+typealias Assets = UIImage
+
+// MARK: - Assets
+
+extension Assets {
+    static let accentColor = Assets(named: "AccentColor")!
+    static let comics22 = Assets(named: "comics22")!
+}
