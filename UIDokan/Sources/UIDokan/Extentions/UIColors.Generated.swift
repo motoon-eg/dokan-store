54d210cf076b7e036debc0845a957725213d40c9
ziad ali <ziadalise@gmail.com>
2022-07-23 16:35:42 +0200

    fix: custom templates

---
+++ UIDokan/Sources/UIDokan/Extentions/UIColors.Generated.swift
@@ -0,0 +1,13 @@
+import UIKit.UIColor
+
+// this is automatic generated file please don't edit it 🔫
+// MARK: - Colors
+
+extension UIColor {
+      static var black: UIColor {
+        UIColor(named: "Black")!
+      } 
+      static var white: UIColor {
+        UIColor(named: "White")!
+      } 
+  }
---
+++ UIDokan/Sources/UIDokan/Extentions/UIImage.Generated.swift
@@ -0,0 +1,16 @@
+import UIKit.UIImage
+
+// this is automatic generated file please don't edit it 🔫
+// MARK: - Images
+
+extension UIImage {
+    static var accentColor : UIImage {
+      UIImage(named: "AccentColor")!
+    }
+    static var comics22 : UIImage {
+      UIImage(named: "comics22")!
+    }
+    static var image2 : UIImage {
+      UIImage(named: "image2")!
+    }
+}
