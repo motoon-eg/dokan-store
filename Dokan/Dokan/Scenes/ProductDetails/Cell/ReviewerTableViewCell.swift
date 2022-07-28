47e57bbe87726e2756a7e75d0c917b8981d4320a
RaniaZeid <67391043+RaniaZeid@users.noreply.github.com>
2022-07-28 14:25:28 -0700

    create reviwer Table View Nib file

---
+++ Dokan/Dokan/Scenes/ProductDetails/Cell/ReviewerTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  ReviewerTableViewCell.swift
+//  Dokan
+//
+//  Created by raniazeid on 28/07/2022.
+//
+
+import UIKit
+
+class ReviewerTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
