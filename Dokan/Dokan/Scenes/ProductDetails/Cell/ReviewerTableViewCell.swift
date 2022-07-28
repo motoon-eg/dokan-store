f661127e4309b5673bd3f69d28dce1a6a9b6c399
RaniaZeid <67391043+RaniaZeid@users.noreply.github.com>
2022-07-28 14:50:21 -0700

    add outlets and cell model

--- Dokan/Dokan/Scenes/ProductDetails/Cell/ReviewerTableViewCell.swift
+++ Dokan/Dokan/Scenes/ProductDetails/Cell/ReviewerTableViewCell.swift
@@ -9,15 +9,29 @@ import UIKit
 
 class ReviewerTableViewCell: UITableViewCell {
 
+   @IBOutlet weak var reviewerImageView: UIImageView!
+    
+    @IBOutlet weak var reviewerNameLabel: UILabel!
+    
+    @IBOutlet weak var reviewerComment: UITextView!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+    }
+    //MARK: - method to configure cell
+    func configureCell(reviewTableViewCellViewModel:ReviewTableViewCellViewModel){
+        reviewerNameLabel.text = reviewTableViewCellViewModel.reviewerName
+        reviewerComment.text = reviewTableViewCellViewModel.reviewerComment
+        
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
     }
     
 }
+struct ReviewTableViewCellViewModel{
+     var reviewerImage : String
+     var reviewerName : String
+     var reviewerComment : String
+ }
