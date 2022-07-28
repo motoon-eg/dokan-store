663a7dee17fbb3b217c2532ad4b035a68d49afa8
claramounir <claramounir555@gmail.com>
2022-07-28 19:34:11 +0200

    solve review notes

---
+++ Dokan/Dokan/Scenes/ProductDetails/UIModels.swift
@@ -0,0 +1,19 @@
+//
+//  UIModels.swift
+//  Dokan
+//
+//  Created by Clara Mounir Adly on 28/07/2022.
+//
+
+import Foundation
+
+struct Review{
+    
+}
+struct ReviewsUiModel{
+    var reviewsCount: Int
+    var rate: Double
+    var reviews: [Review]
+    
+}
+
