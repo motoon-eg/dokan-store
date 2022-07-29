9fbfcaa086976306113342476963c82d0be8e58a
omarradwan <oradwan037@gmail.com>
2022-07-29 18:01:18 +0200

    Update UILabel+Extension.swift
    This reverts commit 03d72709194fb9525e9653eea5fd74bd0c8a72ec.

--- UIDokan/Sources/UIDokan/Extensions/UILabel+Extension.swift
+++ UIDokan/Sources/UIDokan/Extensions/UILabel+Extension.swift
@@ -12,7 +12,7 @@ public extension UILabel {
     func addTrailing(image: UIImage?, text:String) {
         let attachment = NSTextAttachment()
         attachment.image = image
-        attachment.bounds = CGRect(x: 0, y: -5, width: (attachment.image?.size.width)!, height: (attachment.image?.size.height)!)
+        attachment.bounds = CGRect(x: 0, y: -5, width: attachment.image?.size.width ?? 0.0, height: attachment.image?.size.height ?? 0.0)
 
         let attachmentString = NSAttributedString(attachment: attachment)
         let string = NSMutableAttributedString(string: text, attributes: [:])
@@ -21,10 +21,10 @@ public extension UILabel {
         self.attributedText = string
     }
     
-    func addLeading(image: UIImage, text:String) {
+    func addLeading(image: UIImage?, text:String) {
         let attachment = NSTextAttachment()
         attachment.image = image
-        attachment.bounds = CGRect(x: 0, y: -5, width: (attachment.image?.size.width)!, height: (attachment.image?.size.height)!)
+        attachment.bounds = CGRect(x: 0, y: -5, width: attachment.image?.size.width ?? 0.0, height: attachment.image?.size.height ?? 0.0)
 
         let attachmentString = NSAttributedString(attachment: attachment)
         let mutableAttributedString = NSMutableAttributedString()
