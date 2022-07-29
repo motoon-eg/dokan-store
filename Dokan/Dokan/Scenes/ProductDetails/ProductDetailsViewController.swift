497e48786dfe24e89c9d9a2fca8eb63bec21a63d
Ahmed Nagy <ahmednaagy97@gmail.com>
2022-07-29 15:24:04 +0200

    add buttons view

--- Dokan/Dokan/Scenes/ProductDetails/ProductDetailsViewController.swift
+++ Dokan/Dokan/Scenes/ProductDetails/ProductDetailsViewController.swift
@@ -10,27 +10,56 @@ import UIKit
 class ProductDetailsViewController: UIViewController {
     
     // MARK: Outlets
-
+    
+    @IBOutlet weak var buttonsView: ButtonsView!
+    public var questionView: ButtonsView! {
+        guard isViewLoaded else { return nil }
+        return (view as! ButtonsView)
+    }
+    
     // MARK: Properties
-        
+    
     private let viewModel: ProductDetailsViewModelType
-
+    private var addingToCart = false {
+        didSet {
+            if #available(iOS 15.0, *) {
+                questionView.addToCartButton.setNeedsUpdateConfiguration()
+            } else {
+                // Fallback on earlier versions
+            }
+        }
+    }
+    
+    private var addingToFavorite = false {
+        didSet {
+            if #available(iOS 15.0, *) {
+                questionView.addToFavoriteButton.setNeedsUpdateConfiguration()
+            } else {
+                // Fallback on earlier versions
+            }
+        }
+    }
+    
     // MARK: Init
-        
+    //
     init(viewModel: ProductDetailsViewModelType) {
         self.viewModel = viewModel
         super.init(nibName: nil, bundle: nil)
     }
-
+    
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-
+    
     // MARK: Lifecycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupAddToCartButton()
+        setupAddToFavoriteButton()
     }
+    
+    
 }
 
 // MARK: - Actions
@@ -42,10 +71,91 @@ extension ProductDetailsViewController {
 // MARK: - Configurations
 //
 extension ProductDetailsViewController {
+    func setupAddToCartButton() {
+        if #available(iOS 15.0, *) {
+            var config = UIButton.Configuration.filled()
+            config.buttonSize = .large
+            config.cornerStyle = .medium
+            config.titleTextAttributesTransformer = UIConfigurationTextAttributesTransformer { incoming in
+                var outgoing = incoming
+                outgoing.font = UIFont.preferredFont(forTextStyle: .headline)
+                return outgoing
+            }
+            config.image = UIImage(systemName: "chevron.right")
+            config.imagePadding = 5
+            config.imagePlacement = .trailing
+            config.preferredSymbolConfigurationForImage = UIImage.SymbolConfiguration(scale: .medium)
+            
+            buttonsView.addToCartButton.addAction(UIAction { _ in
+                
+                self.addingToCart = true
+                
+                Timer.scheduledTimer(withTimeInterval: 1, repeats: false) { _ in
+                    self.addingToCart = true
+                }
+                
+            }, for: .touchUpInside)
+            
+            buttonsView.addToCartButton.configurationUpdateHandler = { [unowned self] button in
+                
+                var config = button.configuration
+                config?.showsActivityIndicator = self.addingToCart
+                config?.imagePlacement = self.addingToCart ? .leading : .trailing
+                config?.title = self.addingToCart ? "Adding to cart..." : "Add to cart"
+                
+                button.isEnabled = !self.addingToCart
+                button.configuration = config
+            }
+            
+            buttonsView.addToCartButton.configuration = config
+        } else {
+            // Fallback on earlier versions
+        }
+    }
     
+    func setupAddToFavoriteButton() {
+        if #available(iOS 15.0, *) {
+            var config = UIButton.Configuration.filled()
+            config.buttonSize = .large
+            config.cornerStyle = .medium
+            config.titleTextAttributesTransformer = UIConfigurationTextAttributesTransformer { incoming in
+                var outgoing = incoming
+                outgoing.font = UIFont.preferredFont(forTextStyle: .headline)
+                return outgoing
+            }
+            config.image = UIImage(systemName: "heart")
+            config.imagePadding = 5
+            config.imagePlacement = .trailing
+            config.preferredSymbolConfigurationForImage = UIImage.SymbolConfiguration(scale: .medium)
+            
+            buttonsView.addToCartButton.addAction(UIAction { _ in
+                self.addingToFavorite = true
+                
+                Timer.scheduledTimer(withTimeInterval: 1, repeats: false) { _ in
+                    self.addingToFavorite = true
+                }
+            }, for: .touchUpInside)
+            
+            buttonsView.addToFavoriteButton.configurationUpdateHandler = { [unowned self] button in
+                
+                var config = button.configuration
+                config?.showsActivityIndicator = self.addingToFavorite
+                config?.imagePlacement = self.addingToFavorite ? .leading : .trailing
+                config?.image = UIImage(systemName: "heart.fill")
+                config?.title = self.addingToFavorite ? "Adding to favorite..." : "Add to cart"
+                
+                button.isEnabled = !self.addingToFavorite
+                button.configuration = config
+            }
+            
+            buttonsView.addToFavoriteButton.configuration = config
+        } else {
+            // Fallback on earlier versions
+        }
+    }
 }
-
 // MARK: - Private Handlers
 //
 private extension ProductDetailsViewController {
+    
 }
