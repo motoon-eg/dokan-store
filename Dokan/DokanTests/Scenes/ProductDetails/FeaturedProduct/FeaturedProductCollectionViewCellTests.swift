b09e37ca21271a29ec3d56d05fac52d955361fec
Ahmed M. Hassan <ahmdmhasn@gmail.com>
2022-08-03 18:57:40 +0200

    Fix missing files

--- Dokan/DokanTests/Scenes/ProductDetails/FeaturedProduct/FeaturedProductCollectionViewCellTests.swift
+++ Dokan/DokanTests/Scenes/ProductDetails/FeaturedProduct/FeaturedProductCollectionViewCellTests.swift
@@ -5,8 +5,8 @@
 //  Created by marwa on 27/07/2022.
 //
 
-import XCTest
 @testable import Dokan
+import XCTest
 
 class FeaturedProductCollectionViewCellTests: XCTestCase {
 
@@ -32,49 +32,49 @@ class FeaturedProductCollectionViewCellTests: XCTestCase {
         // Then
         XCTAssertNotNil(featuredProductCellProductImage)
     }
-    
+
     func testFeaturedProductCellRateImage_WhenViewIsLoaded_OutletIsLinked() {
         // Given
         let featuredProductCellRateImage = sut.rateImage
         // Then
         XCTAssertNotNil(featuredProductCellRateImage)
     }
-    
+
     func testFeaturedProductCellRateNumber_WhenViewIsLoaded_OutletIsLinked() {
         // Given
         let featuredProductCellRateNumber = sut.rateNumber
         // Then
         XCTAssertNotNil(featuredProductCellRateNumber)
     }
-    
+
     func testFeaturedProductCellProductName_WhenViewIsLoaded_OutletIsLinked() {
         // Given
         let featuredProductCellProductName = sut.productName
         // Then
         XCTAssertNotNil(featuredProductCellProductName)
     }
-    
+
     func testFeaturedProductCellProductPrice_WhenViewIsLoaded_OutletIsLinked() {
         // Given
         let featuredProductCellProductPrice = sut.productPrice
         // Then
         XCTAssertNotNil(featuredProductCellProductPrice)
     }
-    
+
     func testFeaturedProductCellReviewsNumber_WhenViewIsLoaded_OutletIsLinked() {
         // Given
         let featuredProductCellReviewsNumber = sut.reviewsNumber
         // Then
         XCTAssertNotNil(featuredProductCellReviewsNumber)
     }
-    
+
     func testFeaturedProductCellMoreButton_WhenViewIsLoaded_OutletIsLinked() {
         // Given
         let moreButton = sut.moreButton
         // Then
         XCTAssertNotNil(moreButton)
     }
-    
+
     func testMoreButton_WhenViewInitialized_ShouldBeAddedAsTarget() {
         // Given
         let moreButton = sut.moreButton
@@ -82,6 +82,4 @@ class FeaturedProductCollectionViewCellTests: XCTestCase {
         let targets = moreButton?.allTargets
         XCTAssertEqual(targets?.count, 1)
     }
-
 }
-
