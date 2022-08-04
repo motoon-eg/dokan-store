951c1da9590ca13ed980980f602b986aa1de6138
ziad ali <ziadalise@gmail.com>
2022-08-04 13:04:26 +0200

    Add Alert controller

---
+++ UIDokan/Sources/UIDokan/CustomTemplates/AlertController.swift
@@ -0,0 +1,116 @@
+//
+//  File.swift
+//  
+//
+//  Created by ziad on 04/08/2022.
+//
+
+import UIKit
+
+/*
+ * This extension implements a Builder pattern to use with the UIAlertViewController.
+ * Sample:
+ * UIAlertController.Builder()
+ *    .withTitle("this_is_a_key_from_localizable.strings")
+ *    .withMessage("This is a normal message")
+ *    .addOkAction()
+ *    .show(in: UIViewController) OR .build() to get back the UIAlertController directly
+ */
+
+extension UIAlertController {
+    
+    static func okAlert(withTitle title:String? = nil, withMessage message:String? = nil , viewController: UIViewController)  {
+        UIAlertController.Builder()
+            .withTitle(title)
+            .withMessage(message)
+            .addOkAction()
+            .show(in: viewController, animated: true)
+    }
+    
+    /// Builder Pattern to show Alert
+    ///
+    class Builder {
+        private var preferredStyle:UIAlertController.Style = .alert
+        private var title:String? = nil
+        private var message:String? = nil
+        private var actions:[UIAlertAction] = [UIAlertAction]()
+        private var popoverSourceView:UIView? = nil
+        private var sourceRect:CGRect? = nil
+        
+        init() { }
+        
+        func preferredStyle(_ style:UIAlertController.Style) -> Builder {
+            self.preferredStyle = style
+            return self
+        }
+        
+        func withTitle(_ title:String?) -> Builder {
+            self.title = unwrapString(title)
+            return self
+        }
+        
+        func withMessage(_ message:String?) -> Builder {
+            self.message = unwrapString(message)
+            return self
+        }
+        
+        func withPopoverSourceView(_ view:UIView?) -> Builder {
+            self.popoverSourceView = view
+            return self
+        }
+        
+        func addOkAction(handler:((UIAlertAction) ->Void)? = nil) -> Builder {
+            return addDefaultActionWithTitle("OK", handler: handler)
+        }
+        
+        func addDeleteAction(handler:((UIAlertAction) -> Void)? = nil) -> Builder {
+            return addDestructiveActionWithTitle("Delete", handler: handler)
+        }
+        
+        func addCancelAction(handler:((UIAlertAction) -> Void)? = nil) -> Builder {
+            return addCancelActionWithTitle("Cancel", handler: handler)
+        }
+        
+        func addDefaultActionWithTitle(_ title:String, handler:((UIAlertAction) -> Void)? = nil) -> Builder {
+            return addActionWithTitle(title, style: .default, handler: handler)
+        }
+        
+        func addDestructiveActionWithTitle(_ title:String, handler:((UIAlertAction) -> Void)? = nil) -> Builder {
+            return addActionWithTitle(title, style: .destructive, handler: handler)
+        }
+        
+        func addCancelActionWithTitle(_ title:String, handler:((UIAlertAction) -> Void)? = nil) -> Builder {
+            return addActionWithTitle(title, style: .cancel, handler: handler)
+        }
+        
+        func addActionWithTitle(_ title:String, style:UIAlertAction.Style, handler:((UIAlertAction) -> Void)?) -> Builder {
+            let action = UIAlertAction(title: NSLocalizedString(title, comment: ""), style: style, handler: handler)
+            actions.append(action)
+            return self
+        }
+        
+        func show(in viewController:UIViewController, animated:Bool = true, completion:(() -> Void)? = nil) {
+            viewController.present(build(), animated: animated, completion: completion)
+        }
+        
+        private func build() -> UIAlertController {
+            let alert = UIAlertController(title: self.title, message: self.message, preferredStyle: self.preferredStyle)
+            
+            if let popoverSourceView = self.popoverSourceView {
+                alert.popoverPresentationController?.sourceView = popoverSourceView
+            }
+            
+            
+            actions.forEach { (action) in
+                alert.addAction(action)
+            }
+            
+            return alert
+        }
+        
+        private func unwrapString(_ value:String?) -> String? {
+            guard let value = value else { return nil }
+            return value
+        }
+    }
+}
