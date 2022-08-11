3186668f6b39d4948d7a16a68cf4184c6c224133
Ahmed M. Hassan <ahmdmhasn@gmail.com>
2022-08-12 01:11:31 +0200

    Add `ProductUseCase`

--- Domain/Sources/Domain/Model/Product.swift
+++ Domain/Sources/Domain/Model/Product.swift
@@ -1,6 +1,6 @@
 //
 //  Product.swift
-//  
+//
 //
 //  Created by Ahmed M. Hassan on 12/08/2022.
 //
@@ -8,14 +8,14 @@
 import Foundation
 
 /// Define product entity requirements
-/// 
-struct Product {
+///
+public struct Product {
     public let id: String
     public let title: String
     public let description: String
     public let thumbnailUrl: String
     public let imagesListUrls: [String]
-    
+
     public init(id: String, title: String, description: String, thumbnailUrl: String, imagesListUrls: [String]) {
         self.id = id
         self.title = title
---
+++ Domain/Sources/Domain/UseCases/ProductUseCase.swift
@@ -0,0 +1,17 @@
+//
+//  ProductUseCase.swift
+//  
+//
+//  Created by Ahmed M. Hassan on 12/08/2022.
+//
+
+import Foundation
+
+/// All product related use cases
+///
+public protocol ProductUseCase {
+    
+    /// Load all products at a page
+    ///
+    func loadProducts(at page: Int) async throws -> [Product]
+}
