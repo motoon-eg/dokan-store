b4e41b6760f2d5a25eafe919906b0220db49827e
Ahmed M. Hassan <ahmdmhasn@gmail.com>
2022-08-12 01:12:22 +0200

    style: Apply SwiftFormat on recently added files 🌱

--- Domain/Sources/Domain/UseCases/ProductUseCase.swift
+++ Domain/Sources/Domain/UseCases/ProductUseCase.swift
@@ -1,6 +1,6 @@
 //
 //  ProductUseCase.swift
-//  
+//
 //
 //  Created by Ahmed M. Hassan on 12/08/2022.
 //
@@ -10,7 +10,7 @@ import Foundation
 /// All product related use cases
 ///
 public protocol ProductUseCase {
-    
+
     /// Load all products at a page
     ///
     func loadProducts(at page: Int) async throws -> [Product]
