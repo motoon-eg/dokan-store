8640b2a809438999290609d0baa362aa18506d38
ziad ali <52021782+ziadali22@users.noreply.github.com>
2022-08-11 14:25:52 +0200

    Adding Caching Helper

---
+++ Caching/Sources/Caching/UserModel.swift
@@ -0,0 +1,13 @@
+//
+//  File.swift
+//
+//
+//  Created by ziad on 11/08/2022.
+//
+
+import UIKit
+
+struct User: Codable {
+    var name: String?
+    var id: Int?
+}
---
+++ Caching/Tests/CachingTests/CachingTests.swift
@@ -0,0 +1,11 @@
+@testable import Caching
+import XCTest
+
+final class CachingTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(Caching().text, "Hello, World!")
+    }
+}
