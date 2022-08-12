a39f0007217690e657091a282a189db7c254876d
Heba MAD <71231086+HebaMad@users.noreply.github.com>
2022-08-12 09:50:44 +0300

    update

--- Networking/Sources/Networking/Remote/ProductsRemote.swift
+++ Networking/Sources/Networking/Remote/ProductsRemote.swift
@@ -11,7 +11,7 @@ import Foundation
 ///
 public protocol ProductsRemoteProtocol {
     func loadAllProducts(completion: @escaping (Result<[Product], Error>) -> Void)
-    func loadSingleProduct(productID:Int,completion: @escaping (Result<[Product], Error>) -> Void)
+    func loadSingleProduct(productID: Int, completion: @escaping (Result<Product, Error>) -> Void)
 }
 
 /// Products: Remote Endpoints
@@ -26,14 +26,12 @@ public class ProductsRemote: Remote, ProductsRemoteProtocol {
 
         enqueue(request, completion: completion)
     }
-    
-    /// Loads single products
+
+    /// Loads all available products
     ///
-    public func loadSingleProduct(productID:Int,completion: @escaping (Result<Product, Error>) -> Void){
+    public func loadSingleProduct(productID: Int, completion: @escaping (Result<Product, Error>) -> Void) {
         let path = "products/\(productID)"
         let request = FakeStoreRequest(method: .get, path: path)
         enqueue(request, completion: completion)
-
-
     }
 }
