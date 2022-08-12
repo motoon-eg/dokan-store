016cbc0f5f7afb52041d56d21d63b660024780b0
Heba MAD <71231086+HebaMad@users.noreply.github.com>
2022-08-12 08:56:36 +0300

    update slider model

--- Dokan/Dokan/Scenes/ProductDetails/SliderView/SliderView.swift
+++ Dokan/Dokan/Scenes/ProductDetails/SliderView/SliderView.swift
@@ -16,7 +16,7 @@ class SliderView: UIView {
 
     // MARK: - Properties
 
-    private var viewModel: [sliderViewModel]?
+    private var viewModel: [SliderCollectionViewCell.sliderViewModel]?
 
     // MARK: - initializer
 
@@ -74,14 +74,4 @@ extension SliderView: UICollectionViewDelegate, UICollectionViewDataSource {
     }
 }
 
-extension UIView {
-    func fixInView(_ container: UIView) {
-        translatesAutoresizingMaskIntoConstraints = false
-        frame = container.frame
-        container.addSubview(self)
-        NSLayoutConstraint(item: self, attribute: .leading, relatedBy: .equal, toItem: container, attribute: .leading, multiplier: 1.0, constant: 0).isActive = true
-        NSLayoutConstraint(item: self, attribute: .trailing, relatedBy: .equal, toItem: container, attribute: .trailing, multiplier: 1.0, constant: 0).isActive = true
-        NSLayoutConstraint(item: self, attribute: .top, relatedBy: .equal, toItem: container, attribute: .top, multiplier: 1.0, constant: 0).isActive = true
-        NSLayoutConstraint(item: self, attribute: .bottom, relatedBy: .equal, toItem: container, attribute: .bottom, multiplier: 1.0, constant: 0).isActive = true
-    }
-}
+
--- Dokan/Dokan/Scenes/ProductDetails/SliderView/SliderViewModel.swift
+++
@@ -1,9 +0,0 @@
-//
-//  SliderViewModel.swift
-//  Dokan
-//
-//  Created by heba isaa on 10/08/2022.
-//
-
-import Foundation
-import UIKit
