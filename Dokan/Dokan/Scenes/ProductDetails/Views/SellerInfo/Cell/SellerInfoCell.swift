ec6a9e8ca1d18ee7f8a94691611f51365a207fff
RaniaZeid <67391043+RaniaZeid@users.noreply.github.com>
2022-08-18 13:52:57 -0700

    create seller info cell nib. file

---
+++ Dokan/Dokan/Scenes/ProductDetails/Views/SellerInfo/Cell/SellerInfoCell.swift
@@ -0,0 +1,16 @@
+//
+//  SellerInfoCell.swift
+//  Dokan
+//
+//  Created by raniazeid on 17/08/2022.
+//
+
+import UIKit
+
+class SellerInfoCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+}
