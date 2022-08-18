b0f666e98a24cd7ace2f33300df81c59c3ebd4e2
RaniaZeid <67391043+RaniaZeid@users.noreply.github.com>
2022-08-18 14:05:58 -0700

    create cell extension model and configure function

--- Dokan/Dokan/Scenes/ProductDetails/Views/SellerInfo/Cell/SellerInfoCell.swift
+++ Dokan/Dokan/Scenes/ProductDetails/Views/SellerInfo/Cell/SellerInfoCell.swift
@@ -8,9 +8,33 @@
 import UIKit
 
 class SellerInfoCell: UICollectionViewCell {
-
+   // MARK: - outlets..
+    @IBOutlet weak private var sellerProductImage: UIImageView!
+    @IBOutlet weak private var productName: UILabel!
+    @IBOutlet weak private var productPrice: UILabel!
+    @IBOutlet weak private var productReviewRate: UILabel!
+    @IBOutlet weak private var productReviewerCount: UILabel!
+    
+    // MARK: - life cycle...
     override func awakeFromNib() {
         super.awakeFromNib()
     }
-
+    // MARK: - func to configure cell with data
+    func configureCell(model : SellerInfoCellModel){
+        self.sellerProductImage.setImage(with: model.sellerProductImage, placeholderImage: UIImage(named:""))
+        self.productName.text  = model.productName
+        self.productPrice.text = model.productPrice
+        self.productReviewRate.text = model.productReviewRate
+        self.productReviewerCount.text = model.productReviewerCount
+    }
+}
+// MARK: - implement cell view model
+extension SellerInfoCell{
+    struct SellerInfoCellModel{
+        var sellerProductImage : String
+        var productName : String
+        var productPrice: String
+        var productReviewRate : String
+        var productReviewerCount : String
+    }
 }
