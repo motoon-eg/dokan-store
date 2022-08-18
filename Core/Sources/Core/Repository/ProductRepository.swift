0df05ee49dc41ecbbf87ae896178977a21dc75d2
Ahmed M. Hassan <45182214+ahmdmhasn@users.noreply.github.com>
2022-08-18 10:33:00 +0200

    doc: Create Clean Architecture Layers with Implementation  (#108)
    * Add product entity
    
    * Add `ProductUseCase`
    
    * style: Apply SwiftFormat on recently added files 🌱
    
    * Replace async await with completion
    
    * Enhance products remote
    
    * Add `Domain` as a dependency to `Core`
    
    * Update access modifiers for Network Product
    
    * Add `DomainConvertible` and conform `Product` to it
    
    * Implement product details
    
    * Add service locator with networking implementation
    
    * Rename `ProductRepository`
    
    * replace user repository
    
    * add repository provider with default implementation
    
    * add repository protocol instead of concrete object
    
    * Update access modifiers

---
+++ Core/Sources/Core/Repository/ProductRepository.swift
@@ -0,0 +1,32 @@
+//
+//  ProductRepository.swift
+//
+//
+//  Created by Ahmed M. Hassan on 12/08/2022.
+//
+
+import Domain
+import Foundation
+import Networking
+
+/// Implementation of `Domain.ProductRepository`
+///
+struct ProductRepository: Domain.ProductRepository {
+    let remote: ProductsRemoteProtocol
+
+    init(remote: ProductsRemoteProtocol) {
+        self.remote = remote
+    }
+
+    func loadProducts(at page: Int, completion: @escaping (Result<[Domain.Product], Error>) -> Void) {
+        remote.loadProducts(at: page) { result in
+            switch result {
+            case let .success(list):
+                let domainList = list.map { $0.toDomain() }
+                completion(.success(domainList))
+            case let .failure(error):
+                completion(.failure(error))
+            }
+        }
+    }
+}
