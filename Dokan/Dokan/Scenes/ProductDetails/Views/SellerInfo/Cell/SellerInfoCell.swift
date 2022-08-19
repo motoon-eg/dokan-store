2fc2b68fbda0f001e004a164da94912a843b9dc4
RaniaZeid <67391043+RaniaZeid@users.noreply.github.com>
2022-08-19 04:52:16 -0700

    solve uiImage problem

--- Dokan/Dokan/Scenes/ProductDetails/Views/SellerInfo/Cell/SellerInfoCell.swift
+++ Dokan/Dokan/Scenes/ProductDetails/Views/SellerInfo/Cell/SellerInfoCell.swift
@@ -8,33 +8,39 @@
 import UIKit
 
 class SellerInfoCell: UICollectionViewCell {
-   // MARK: - outlets..
-    @IBOutlet weak private var sellerProductImage: UIImageView!
-    @IBOutlet weak private var productName: UILabel!
-    @IBOutlet weak private var productPrice: UILabel!
-    @IBOutlet weak private var productReviewRate: UILabel!
-    @IBOutlet weak private var productReviewerCount: UILabel!
-    
+    // MARK: - outlets..
+
+    @IBOutlet private weak var sellerProductImage: UIImageView!
+    @IBOutlet private weak var productName: UILabel!
+    @IBOutlet private weak var productPrice: UILabel!
+    @IBOutlet private weak var productReviewRate: UILabel!
+    @IBOutlet private weak var productReviewerCount: UILabel!
+
     // MARK: - life cycle...
+
     override func awakeFromNib() {
         super.awakeFromNib()
     }
+
     // MARK: - func to configure cell with data
-    func configureCell(model : SellerInfoCellModel){
-        self.sellerProductImage.setImage(with: model.sellerProductImage, placeholderImage: UIImage(named:""))
-        self.productName.text  = model.productName
-        self.productPrice.text = model.productPrice
-        self.productReviewRate.text = model.productReviewRate
-        self.productReviewerCount.text = model.productReviewerCount
+
+    func configureCell(model: SellerInfoCellModel) {
+        sellerProductImage.setImage(with: model.sellerProductImage, placeholderImage: UIImage(named: "")!)
+        productName.text = model.productName
+        productPrice.text = model.productPrice
+        productReviewRate.text = model.productReviewRate
+        productReviewerCount.text = model.productReviewerCount
     }
 }
+
 // MARK: - implement cell view model
-extension SellerInfoCell{
-    struct SellerInfoCellModel{
-        var sellerProductImage : String
-        var productName : String
+
+extension SellerInfoCell {
+    struct SellerInfoCellModel {
+        var sellerProductImage: String
+        var productName: String
         var productPrice: String
-        var productReviewRate : String
-        var productReviewerCount : String
+        var productReviewRate: String
+        var productReviewerCount: String
     }
 }
