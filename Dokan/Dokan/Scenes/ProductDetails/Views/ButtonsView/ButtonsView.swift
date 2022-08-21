840d226f9d574a6f950f6ef745528988a0ce56d8
Ahmed Nagy <ahmednaagy97@gmail.com>
2022-08-21 23:23:23 +0200

    fix: buttonsView compatibility with iOS 13 and above 📈 (#106)
    * add buttons view
    
    * fix ButtonsView not showing and change the favorite button color
    
    * delete the test code in SceneDelegate
    
    * change favorite button color
    
    * make product details screen the root viewController
    
    code added to SceneDelegate to launch the Product Details screen on launch to test out current tasks
    
    * fix: make buttonsView compatible starting from iOS 13 and above
    
    * adding access control to ButtonsView
    
    * resolving minor issues regarding last PR comments
    
    * fixing naming issues
    
    * fix: sellerImageProfile outlets connections
    
    * fix InfoSellerView outlets connections

---
+++ Dokan/Dokan/Scenes/ProductDetails/Views/ButtonsView/ButtonsView.swift
@@ -0,0 +1,33 @@
+//
+//  ButtonsView.swift
+//  Dokan
+//
+//  Created by Ahmed Nagy on 22/07/2022.
+//
+
+import UIKit
+
+class ButtonsView: UIView {
+
+    // Outlets
+    @IBOutlet private var contentView: UIView!
+    @IBOutlet weak var addToFavoriteButton: LoadingButton!
+    @IBOutlet weak var addToCartButton: LoadingButton!
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        initiateButtonsView()
+    }
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        initiateButtonsView()
+    }
+
+    private func initiateButtonsView() {
+        Bundle.main.loadNibNamed("ButtonsView", owner: self, options: nil)
+        addSubview(contentView)
+        contentView.frame = bounds
+        contentView.autoresizingMask = [.flexibleHeight, .flexibleWidth]
+    }
+}
