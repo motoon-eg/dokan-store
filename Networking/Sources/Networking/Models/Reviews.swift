661dfafaccae8a46dc3cf02d3cf190cf2ea40993
MarwaEbrahem <marwa15shanab@gmail.com>
2022-08-23 15:01:44 -0700

    feat: Add mock product reviews files

---
+++ Networking/Sources/Networking/Models/Reviews.swift
@@ -0,0 +1,27 @@
+//
+//  File.swift
+//
+//
+//  Created by marwa on 23/08/2022.
+//
+
+import Foundation
+
+// MARK: - Reviews
+
+public struct Reviews: Codable {
+    public let rating: Double
+    public let totalRatingNumber, fiveStarRatingNumber, fourStarRatingNumber, threeStarRatingNumber: Int
+    public let twoStarRatingNumber, oneStarRatingNumber: Int
+    public let reviews: [Review]
+}
+
+// MARK: - Review
+
+public struct Review: Codable {
+    public let image: String
+    public let name: String
+    public let date: String
+    public let rating: Int
+    public let reviewDescription: String?
+}
---
+++ Networking/Sources/Networking/Network/MockNetwork.swift
@@ -0,0 +1,130 @@
+//
+//  File.swift
+//
+//
+//  Created by marwa on 23/08/2022.
+//
+
+import Foundation
+
+
+public class MockNetwork {
+    
+    public init() {}
+    
+    public func loadReviewProductData(completion: @escaping (Reviews?,Error?) -> Void) {
+        var Reviewsproduct: Reviews
+        do {
+            let allProductData = try JSONSerialization.data(withJSONObject: Constants.mockProductReviewsJsonResponse, options: .fragmentsAllowed)
+            Reviewsproduct = try JSONDecoder().decode(Reviews.self, from: allProductData)
+            completion(Reviewsproduct, nil)
+        } catch {
+            print(error.localizedDescription)
+            completion(nil, error)
+        }
+    }
+}
+
+
+
+extension MockNetwork {
+    
+    private enum Constants {
+        
+        static let mockProductReviewsJsonResponse: [String: Any] = [
+            "rating": 4.5,
+            "totalRatingNumber": 12,
+            "fiveStarRatingNumber": 5,
+            "fourStarRatingNumber": 3,
+            "threeStarRatingNumber": 1,
+            "twoStarRatingNumber": 2,
+            "oneStarRatingNumber": 1,
+            "reviews": [
+                [
+                    "image": " ",
+                    "name": "yacob Jameruddin Debiza",
+                    "date": "2021-05-17",
+                    "rating": 5,
+                    "reviewDescription": "Make a statement with these iconic Dr Marten boots. This classic style has stood the test of time thanks to the combination of reinvented styling and the rugged yet urban look. Crafted with high-quality",
+                ],
+                [
+                    "image": " ",
+                    "name": "yacob Jameruddin Debiza",
+                    "date": "2021-05-17",
+                    "rating": 4,
+                    "reviewDescription": "Make a statement with these iconic Dr Marten boots. This classic style has stood the test of time thanks to the combination of reinvented styling and the rugged yet urban look. Crafted with high-quality",
+                ],
+                [
+                    "image": " ",
+                    "name": "yacob Jameruddin Debiza",
+                    "date": "2021-05-17",
+                    "rating": 3,
+                    "reviewDescription": "Make a statement with these iconic Dr Marten boots. This classic style has stood the test of time thanks to the combination of reinvented styling and the rugged yet urban look. Crafted with high-quality",
+                ],
+                [
+                    "image": " ",
+                    "name": "yacob Jameruddin Debiza",
+                    "date": "2021-05-17",
+                    "rating": 5,
+                    "reviewDescription": "Make a statement with these iconic Dr Marten boots. This classic style has stood the test of time thanks to the combination of reinvented styling and the rugged yet urban look. Crafted with high-quality",
+                ],
+                [
+                    "image": " ",
+                    "name": "yacob Jameruddin Debiza",
+                    "date": "2021-05-17",
+                    "rating": 4,
+                    "reviewDescription": "Make a statement with these iconic Dr Marten boots. This classic style has stood the test of time thanks to the combination of reinvented styling and the rugged yet urban look. Crafted with high-quality",
+                ],
+                [
+                    "image": " ",
+                    "name": "yacob Jameruddin Debiza",
+                    "date": "2021-05-17",
+                    "rating": 2,
+                    "reviewDescription": "Make a statement with these iconic Dr Marten boots. This classic style has stood the test of time thanks to the combination of reinvented styling and the rugged yet urban look. Crafted with high-quality",
+                ],
+                [
+                    "image": " ",
+                    "name": "yacob Jameruddin Debiza",
+                    "date": "2021-05-17",
+                    "rating": 5,
+                    "reviewDescription": "Make a statement with these iconic Dr Marten boots. This classic style has stood the test of time thanks to the combination of reinvented styling and the rugged yet urban look. Crafted with high-quality",
+                ],
+                [
+                    "image": " ",
+                    "name": "yacob Jameruddin Debiza",
+                    "date": "2021-05-17",
+                    "rating": 1,
+                    "reviewDescription": "Make a statement with these iconic Dr Marten boots. This classic style has stood the test of time thanks to the combination of reinvented styling and the rugged yet urban look. Crafted with high-quality",
+                ],
+                [
+                    "image": " ",
+                    "name": "yacob Jameruddin Debiza",
+                    "date": "2021-05-17",
+                    "rating": 4,
+                    "reviewDescription": "Make a statement with these iconic Dr Marten boots. This classic style has stood the test of time thanks to the combination of reinvented styling and the rugged yet urban look. Crafted with high-quality",
+                ],
+                [
+                    "image": " ",
+                    "name": "khaled kashmiry",
+                    "date": "2021-05-17",
+                    "rating": 4,
+                    "reviewDescription": "Make a statement with these iconic Dr Marten boots. This classic style has stood the test of time thanks to the combination of reinvented styling and the rugged yet urban look. Crafted with high-quality",
+                ],
+                [
+                    "image": "https://cdn.shopify.com/s/files/1/0567/9310/4582/products/c872c818621155d8fb535ee9d9c98803.jpg?v=1621288216 ",
+                    "name": "yacob Jameruddin Debiza",
+                    "date": "2021-05-17",
+                    "rating": 5,
+                    "reviewDescription": "Make a statement with these iconic Dr Marten boots. This classic style has stood the test of time thanks to the combination of reinvented styling and the rugged yet urban look. Crafted with high-quality",
+                ],
+                [
+                    "image": "https://cdn.shopify.com/s/files/1/0567/9310/4582/products/ccba62c19289e4b5f14b180c05836614.jpg?v=1621288216 ",
+                    "name": "yacob Jameruddin Debiza",
+                    "date": "2021-05-17",
+                    "rating": 3,
+                    "reviewDescription": "Make a statement with these iconic Dr Marten boots. This classic style has stood the test of time thanks to the combination of reinvented styling and the rugged yet urban look. Crafted with high-quality",
+                ],
+            ],
+        ]
+    }
+}
